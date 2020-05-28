435e9fa1705623ff38f40d71b2cf6d306b1531c4
Henri Meltaus <henri@takomo.io>
2020-05-28 21:06:42 +0300

    Fix tests

--- packages/takomo-stacks-context/test/context/config/build/build-parameters.test.ts
+++ packages/takomo-stacks-context/test/context/config/build/build-parameters.test.ts
@@ -1,20 +1,25 @@
 import { ParameterName } from "@takomo/stacks-model"
-import { coreResolverInitializers } from "@takomo/stacks-resolvers"
+import {
+  coreResolverProviders,
+  ResolverRegistry,
+} from "@takomo/stacks-resolvers"
+import { ConsoleLogger } from "@takomo/util"
 import { buildParameters } from "../../../../src/config/parameters"
 
-const initializers = coreResolverInitializers()
+const registry = new ResolverRegistry(new ConsoleLogger())
+coreResolverProviders().forEach((p) => registry.registerBuiltInProvider(p))
 
 describe("#buildParameters", () => {
   describe("should build correct parameters", () => {
     it("when a an empty config is given", async () => {
       const config = new Map<ParameterName, any>()
-      const parameters = await buildParameters(config, initializers)
+      const parameters = await buildParameters(config, registry)
       expect(parameters.size).toBe(0)
     })
 
     it("when a static string parameter config is given", async () => {
       const config = new Map<ParameterName, any>([["Param1", "string value"]])
-      const parameters = await buildParameters(config, initializers)
+      const parameters = await buildParameters(config, registry)
       expect(parameters.size).toBe(1)
 
       const resolver1 = parameters.get("Param1")
@@ -23,7 +28,7 @@ describe("#buildParameters", () => {
 
     it("when a static number parameter config is given", async () => {
       const config = new Map<ParameterName, any>([["NumberParam", 1234]])
-      const parameters = await buildParameters(config, initializers)
+      const parameters = await buildParameters(config, registry)
       expect(parameters.size).toBe(1)
 
       const resolver1 = parameters.get("NumberParam")
@@ -32,7 +37,7 @@ describe("#buildParameters", () => {
 
     it("when a static boolean parameter config is given", async () => {
       const config = new Map<ParameterName, any>([["BooleanParam", true]])
-      const parameters = await buildParameters(config, initializers)
+      const parameters = await buildParameters(config, registry)
       expect(parameters.size).toBe(1)
 
       const resolver1 = parameters.get("BooleanParam")
@@ -43,7 +48,7 @@ describe("#buildParameters", () => {
       const config = new Map<ParameterName, any>([
         ["SubnetIds", ["subnet1", "subnet2", "subnet3"]],
       ])
-      const parameters = await buildParameters(config, initializers)
+      const parameters = await buildParameters(config, registry)
       expect(parameters.size).toBe(1)
 
       const resolver1 = parameters.get("SubnetIds")
@@ -57,7 +62,7 @@ describe("#buildParameters", () => {
           ["subnet1", 1, true, { resolver: "secret", secret: "password" }],
         ],
       ])
-      const parameters = await buildParameters(config, initializers)
+      const parameters = await buildParameters(config, registry)
       expect(parameters.size).toBe(1)
 
       const resolver1 = parameters.get("SomeValues")
