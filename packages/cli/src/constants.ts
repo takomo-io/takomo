dd1338e5ba410e2e5ba24cdde9bb7be50e8df807
Henri Meltaus <henri@takomo.io>
2021-02-20 15:59:20 +0200

    feat: load supported regions from takomo.yml project configuration file (#160)
    affects: @takomo/cli, @takomo/core
    
    ISSUES CLOSED: #147

--- packages/cli/src/constants.ts
+++ packages/cli/src/constants.ts
@@ -1,3 +1,5 @@
+import { Region } from "@takomo/aws-model"
+
 export const CONFIG_FILE_EXTENSION = ".yml"
 export const TEMPLATES_DIR = "templates"
 export const STACKS_DIR = "stacks"
@@ -12,3 +14,30 @@ export const DEFAULT_DEPLOYMENT_CONFIG_FILE = "targets.yml"
 export const DEFAULT_ORGANIZATION_CONFIG_FILE = "organization.yml"
 export const STACK_GROUP_CONFIG_FILE_NAME = "config.yml"
 export const TAKOMO_PROJECT_CONFIG_FILE_NAME = "takomo.yml"
+
+export const DEFAULT_REGIONS: ReadonlyArray<Region> = [
+  "af-south-1",
+  "ap-east-1",
+  "ap-northeast-1",
+  "ap-northeast-2",
+  "ap-northeast-3",
+  "ap-south-1",
+  "ap-southeast-1",
+  "ap-southeast-2",
+  "ca-central-1",
+  "cn-north-1",
+  "cn-northwest-1",
+  "eu-central-1",
+  "eu-north-1",
+  "eu-south-1",
+  "eu-west-1",
+  "eu-west-2",
+  "eu-west-3",
+  "me-south-1",
+  "sa-east-1",
+  "us-east-1",
+  "us-east-2",
+  "us-gov-east-1",
+  "us-west-1",
+  "us-west-2",
+]
---
+++ packages/cli/test/parse-project-config-file.test.ts
@@ -0,0 +1,35 @@
+import { FilePath } from "@takomo/util"
+import { parseProjectConfigFile } from "../src/config"
+import { DEFAULT_REGIONS } from "../src/constants"
+
+const doParseProjectConfigFile = (pathToFile: FilePath): any =>
+  parseProjectConfigFile(`${process.cwd()}/test/${pathToFile}`)
+
+describe("#parseProjectConfigFile", () => {
+  test("with empty file", async () => {
+    const config = await doParseProjectConfigFile("project-config-01.yml")
+    expect(config).toStrictEqual({
+      requiredVersion: undefined,
+      organization: undefined,
+      regions: DEFAULT_REGIONS,
+    })
+  })
+
+  test("with a single region", async () => {
+    const config = await doParseProjectConfigFile("project-config-02.yml")
+    expect(config).toStrictEqual({
+      requiredVersion: undefined,
+      organization: undefined,
+      regions: ["eu-west-1"],
+    })
+  })
+
+  test("with multiple regions", async () => {
+    const config = await doParseProjectConfigFile("project-config-03.yml")
+    expect(config).toStrictEqual({
+      requiredVersion: undefined,
+      organization: undefined,
+      regions: ["eu-central-1", "eu-north-1", "us-east-1"],
+    })
+  })
+})
