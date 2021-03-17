2989544a5611cb7fafa7cb990b43dd6558544f59
Henri Meltaus <henri@takomo.io>
2021-03-17 22:00:12 +0200

    Changes to tests

--- integration-tests/deployment-targets/test/deploy-with-deployment-role.test.ts
+++ integration-tests/deployment-targets/test/deploy-with-deployment-role.test.ts
@@ -5,62 +5,53 @@
 import {
   executeDeployTargetsCommand,
   executeUndeployTargetsCommand,
-  TIMEOUT,
 } from "@takomo/test-integration"
 
-const projectDir = "configs"
+const projectDir = "configs/simple"
 
 describe("Deployment with deployment role", () => {
-  test(
-    "deploy all",
-    async () => {
-      const { results } = await executeDeployTargetsCommand({
-        projectDir,
-        logLevel: "trace",
-        configFile: "targets-2.yml",
-      })
-        .expectCommandToSucceed()
-        .assert()
+  test("deploy all", async () => {
+    const { results } = await executeDeployTargetsCommand({
+      projectDir,
+      logLevel: "trace",
+      configFile: "targets-2.yml",
+    })
+      .expectCommandToSucceed()
+      .assert()
 
-      expect(results).toHaveLength(1)
+    expect(results).toHaveLength(1)
 
-      const [exampleGroup] = results
+    const [exampleGroup] = results
 
-      expect(exampleGroup.path).toBe("Example")
-      expect(exampleGroup.results).toHaveLength(2)
-      expect(exampleGroup.success).toBeTruthy()
-      expect(exampleGroup.status).toBe("SUCCESS")
+    expect(exampleGroup.path).toBe("Example")
+    expect(exampleGroup.results).toHaveLength(2)
+    expect(exampleGroup.success).toBeTruthy()
+    expect(exampleGroup.status).toBe("SUCCESS")
 
-      const [t1, t2] = exampleGroup.results
-      expect(t1.name).toBe("foo")
-      expect(t2.name).toBe("bar")
-    },
-    TIMEOUT,
-  )
+    const [t1, t2] = exampleGroup.results
+    expect(t1.name).toBe("foo")
+    expect(t2.name).toBe("bar")
+  })
 
-  test(
-    "undeploy all",
-    async () => {
-      const { results } = await executeUndeployTargetsCommand({
-        projectDir,
-        configFile: "targets-2.yml",
-      })
-        .expectCommandToSucceed()
-        .assert()
+  test("undeploy all", async () => {
+    const { results } = await executeUndeployTargetsCommand({
+      projectDir,
+      configFile: "targets-2.yml",
+    })
+      .expectCommandToSucceed()
+      .assert()
 
-      expect(results).toHaveLength(1)
+    expect(results).toHaveLength(1)
 
-      const [exampleGroup] = results
+    const [exampleGroup] = results
 
-      expect(exampleGroup.path).toBe("Example")
-      expect(exampleGroup.results).toHaveLength(2)
-      expect(exampleGroup.success).toBeTruthy()
-      expect(exampleGroup.status).toBe("SUCCESS")
+    expect(exampleGroup.path).toBe("Example")
+    expect(exampleGroup.results).toHaveLength(2)
+    expect(exampleGroup.success).toBeTruthy()
+    expect(exampleGroup.status).toBe("SUCCESS")
 
-      const [t1, t2] = exampleGroup.results
-      expect(t1.name).toBe("foo")
-      expect(t2.name).toBe("bar")
-    },
-    TIMEOUT,
-  )
+    const [t1, t2] = exampleGroup.results
+    expect(t1.name).toBe("foo")
+    expect(t2.name).toBe("bar")
+  })
 })
