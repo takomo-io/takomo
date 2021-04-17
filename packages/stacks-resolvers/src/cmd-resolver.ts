f51e7ce68bf8babdb83254d0d152d9c7180416be
Henri Meltaus <henri@takomo.io>
2021-04-17 22:02:48 +0300

    feat(resolvers): add option to expose stack region and credentials to cmd resolver
    affects: @takomo/stacks-resolvers
    
    ISSUES CLOSED: #211

--- packages/stacks-resolvers/src/cmd-resolver.ts
+++ packages/stacks-resolvers/src/cmd-resolver.ts
@@ -4,25 +4,54 @@ import {
   ResolverProvider,
   ResolverProviderSchemaProps,
 } from "@takomo/stacks-model"
-import { deepFreeze } from "@takomo/util"
+import { deepCopy, deepFreeze } from "@takomo/util"
 import { exec } from "child_process"
 import { ObjectSchema } from "joi"
 import { promisify } from "util"
 
 const execP = promisify(exec)
 
-export const init = async ({ command }: any): Promise<Resolver> => {
+export const init = async ({
+  command,
+  exposeStackCredentials,
+  exposeStackRegion,
+  cwd,
+}: any): Promise<Resolver> => {
   if (!command) {
     throw new Error("command is required property")
   }
 
   return deepFreeze({
-    resolve: async ({ logger, parameterName }: ResolverInput): Promise<any> => {
+    resolve: async ({
+      logger,
+      parameterName,
+      stack,
+      ctx,
+    }: ResolverInput): Promise<any> => {
       logger.debug(
         `Resolving value for parameter '${parameterName}' with command: ${command}`,
       )
-      const { stdout } = await execP(command)
-      return (stdout || "").trim()
+
+      const env = deepCopy(process.env)
+
+      if (exposeStackCredentials === true) {
+        const credentials = await stack.credentialManager.getCredentials()
+        env.AWS_ACCESS_KEY_ID = credentials.accessKeyId
+        env.AWS_SECRET_ACCESS_KEY = credentials.secretAccessKey
+        env.AWS_SESSION_TOKEN = credentials.sessionToken
+        env.AWS_SECURITY_TOKEN = credentials.sessionToken
+      }
+
+      if (exposeStackRegion === true) {
+        env.AWS_DEFAULT_REGION = stack.region
+      }
+
+      const { stdout } = await execP(command, {
+        cwd: cwd ?? ctx.projectDir,
+        env,
+      })
+
+      return (stdout ?? "").trim()
     },
   })
 }
@@ -32,6 +61,9 @@ const name = "cmd"
 const schema = ({ joi, base }: ResolverProviderSchemaProps): ObjectSchema =>
   base.keys({
     command: joi.string().required(),
+    exposeStackCredentials: joi.boolean(),
+    exposeStackRegion: joi.boolean(),
+    cwd: joi.string(),
   })
 
 export const createCmdResolverProvider = (): ResolverProvider =>
