f930fd9609b5a180d70d78a459a952f283b16a12
Henri Meltaus <henri@takomo.io>
2021-06-27 20:02:49 +0300

    feat(stacks): do not validate schemas in stack groups (#261)
    affects: integration-test-stacks, @takomo/stacks-context

--- integration-tests/stacks/test/schemas.test.ts
+++ integration-tests/stacks/test/schemas.test.ts
@@ -5,21 +5,6 @@ const stackPath = "/stack1.yml/eu-west-1",
   projectDir = "configs/schemas"
 
 describe("Schemas", () => {
-  test("Validation errors in stack group data", () =>
-    executeDeployStacksCommand({
-      projectDir,
-      var: [
-        "age=50",
-        "greeting=hello",
-        "environment=dev",
-        "stackGroupCostCenter=code",
-        "stackCostCenter=code",
-      ],
-    }).expectCommandToThrow(
-      "Validation errors in data configuration of stack group /:\n\n" +
-        '  - "age" must be less than or equal to 40',
-    ))
-
   test("No validation errors", () =>
     executeDeployStacksCommand({
       projectDir,
@@ -48,21 +33,6 @@ describe("Schemas", () => {
     }).expectCommandToThrow(
       "Validation errors in data configuration of stack /stack1.yml/eu-west-1:\n\n" +
         '  - "greeting" length must be less than or equal to 5 characters long',
-    ))
-
-  test("Validation errors in stack group tags", () =>
-    executeDeployStacksCommand({
-      projectDir,
-      var: [
-        "age=40",
-        "greeting=good-day",
-        "environment=dev",
-        "stackGroupCostCenter=xxxxxxx",
-        "stackCostCenter=code",
-      ],
-    }).expectCommandToThrow(
-      "Validation errors in tags configuration of stack group /:\n\n" +
-        '  - "costCenter" length must be less than or equal to 4 characters long',
     ))
 
   test("Validation errors in stack tags", () =>
