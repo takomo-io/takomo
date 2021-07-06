816514e8c84a4121f2a7ee99e784e3ee5da82ad0
Henri Meltaus <henri@takomo.io>
2021-07-06 20:29:39 +0300

    feat(deployment targets): add option to infer deployment group path from target's file path (#264)
    affects: integration-test-deployment-targets, @takomo/deployment-targets-repository

---
+++ packages/deployment-targets-repository/src/errors.ts
@@ -0,0 +1,18 @@
+import { FilePath, TakomoError } from "@takomo/util"
+
+export class InvalidDeploymentTargetFileLocationError extends TakomoError {
+  constructor(pathToFile: FilePath, baseDir: FilePath) {
+    super(
+      `Deployment target file ${pathToFile} must not be directly in ${baseDir} dir`,
+      {
+        instructions: [
+          `Create a subdirectory under ${baseDir} and move the deployment target file there`,
+        ],
+        info:
+          "When 'inferDeploymentGroupPathFromDirName' property is set to true, " +
+          "deployment target files can't be placed directory into the dir specified " +
+          "with the 'dir' property",
+      },
+    )
+  }
+}
