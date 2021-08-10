f3b4e838ab76d33db2ad18f888dbb2c8c7d0223b
Henri Meltaus <henri@takomo.io>
2021-08-10 21:39:20 +0300

    refactor

--- integration-tests/stacks2/test/tag-inheritance.test.ts
+++ integration-tests/stacks2/test/tag-inheritance.test.ts
@@ -1,66 +1,67 @@
-import { aws, executeDeployStacksCommand } from "@takomo/test-integration"
-import { Credentials } from "aws-sdk"
+import {
+  executeDeployStacksCommand,
+  withSingleAccountReservation,
+} from "@takomo/test-integration"
 
 describe("Tag inheritance", () => {
-  test("Deploy", async () => {
-    await executeDeployStacksCommand({
-      projectDir: "configs/tag-inheritance",
-    })
-      .expectCommandToSucceed()
-      .expectStackCreateSuccess({
-        stackName: "three",
-        stackPath: "/three.yml/eu-north-1",
+  test(
+    "Deploy",
+    withSingleAccountReservation(({ credentials, accountId }) =>
+      executeDeployStacksCommand({
+        projectDir: "configs/tag-inheritance",
       })
-      .expectStackCreateSuccess({
-        stackName: "aaa-two",
-        stackPath: "/aaa/two.yml/eu-north-1",
-      })
-      .expectStackCreateSuccess({
-        stackName: "aaa-bbb-one",
-        stackPath: "/aaa/bbb/one.yml/eu-north-1",
-      })
-      .assert()
-
-    const credentials = new Credentials(global.reservation.credentials)
-    const iamRoleArn = `arn:aws:iam::${global.reservation.accounts[0].accountId}:role/OrganizationAccountAccessRole`
-
-    const three = await aws.cloudFormation.describeStack({
-      credentials,
-      iamRoleArn,
-      region: "eu-north-1",
-      stackName: "three",
-    })
-
-    expect(three.Tags).toStrictEqual([
-      { Key: "foo", Value: "bar" },
-      { Key: "fux", Value: "baz" },
-      { Key: "hello", Value: "world" },
-    ])
-
-    const two = await aws.cloudFormation.describeStack({
-      credentials,
-      iamRoleArn,
-      region: "eu-north-1",
-      stackName: "aaa-two",
-    })
-
-    expect(two.Tags).toStrictEqual([
-      { Key: "foo", Value: "bar1" },
-      { Key: "fux", Value: "baz" },
-      { Key: "hello", Value: "world" },
-    ])
-
-    const one = await aws.cloudFormation.describeStack({
-      credentials,
-      iamRoleArn,
-      region: "eu-north-1",
-      stackName: "aaa-bbb-one",
-    })
-
-    expect(one.Tags).toStrictEqual([
-      { Key: "foo", Value: "bar1" },
-      { Key: "fux", Value: "new-value" },
-      { Key: "hello", Value: "world" },
-    ])
-  })
+        .expectCommandToSucceed()
+        .expectStackCreateSuccess(
+          {
+            stackName: "three",
+            stackPath: "/three.yml/eu-north-1",
+          },
+          {
+            stackName: "aaa-two",
+            stackPath: "/aaa/two.yml/eu-north-1",
+          },
+          {
+            stackName: "aaa-bbb-one",
+            stackPath: "/aaa/bbb/one.yml/eu-north-1",
+          },
+        )
+        .expectDeployedCfStack({
+          credentials,
+          accountId,
+          stackName: "three",
+          region: "eu-north-1",
+          roleName: "OrganizationAccountAccessRole",
+          expectedTags: {
+            foo: "bar",
+            fux: "baz",
+            hello: "world",
+          },
+        })
+        .expectDeployedCfStack({
+          credentials,
+          accountId,
+          stackName: "aaa-two",
+          region: "eu-north-1",
+          roleName: "OrganizationAccountAccessRole",
+          expectedTags: {
+            foo: "bar1",
+            fux: "baz",
+            hello: "world",
+          },
+        })
+        .expectDeployedCfStack({
+          credentials,
+          accountId,
+          stackName: "aaa-bbb-one",
+          region: "eu-north-1",
+          roleName: "OrganizationAccountAccessRole",
+          expectedTags: {
+            foo: "bar1",
+            fux: "new-value",
+            hello: "world",
+          },
+        })
+        .assert(),
+    ),
+  )
 })
