72f2cd68461f1418c178164cc373c88f9b357d22
Henri Meltaus <henri@takomo.io>
2021-09-12 19:46:45 +0300

    fix(organization): fix list accounts stacks iam policies

--- packages/organization-commands/src/accounts/list-stacks/iam-policy.ts
+++ packages/organization-commands/src/accounts/list-stacks/iam-policy.ts
@@ -1,8 +1,31 @@
 export const listAccountsStacksCommandIamPolicy = (): string => `
 Statement:
-  - Effect: Allow
-    Action:
+
+  # IAM permissions required for the organization master account.
+  - Action:
       - organizations:DescribeOrganization
+      - organizations:DescribePolicy
+      - organizations:ListAWSServiceAccessForOrganization
       - organizations:ListAccounts
-    Resource: "*"
+      - organizations:ListAccountsForParent
+      - organizations:ListOrganizationalUnitsForParent
+      - organizations:ListPolicies
+      - organizations:ListRoots
+      - organizations:ListTargetsForPolicy
+    Effect: Allow
+    Resource: '*'
+    Sid: organizations
+  - Action:
+      - sts:AssumeRole
+    Effect: Allow
+    Resource: '*'
+    Sid: sts
+    
+  # IAM permissions required in each organization account.
+  - Sid: IAM
+    Effect: Allow
+    Action:
+      - sts:AssumeRole
+      - cloudformation:ListStacks
+    Resource: "*"  
 `
