bfefb43f723c881bee5c93c553931a988bec7cfc
Henri Meltaus <henri@takomo.io>
2021-10-14 10:34:05 +0300

    chore: upgrade dependencies (#319)

--- packages/util/src/objects.ts
+++ packages/util/src/objects.ts
@@ -3,23 +3,12 @@ import _merge from "lodash.merge"
 /**
  * @hidden
  */
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const deepFreezeNode = require("deep-freeze-node")
-
-/**
- * @hidden
- */
 export const deepCopy = (obj: any): any => JSON.parse(JSON.stringify(obj))
 
 /**
  * @hidden
  */
 export const identity = <T>(value: T): T => value
-
-/**
- * @hidden
- */
-export const deepFreeze = <T>(obj: T): T => deepFreezeNode(obj)
 
 /**
  * @hidden
