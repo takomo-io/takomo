2c1e7ef78c8f4a6968b15bef0cc8c29e849b1c55
Henri Meltaus <henri@takomo.io>
2022-01-17 20:25:33 +0200

    fix: fix diffing of stack policies
    affects: @takomo/cli-io
    
    Stack policies were incorrectly compared as text when the right way would have
    been comparing them
    as objects. Due to this, two policies with identical
    properties but in a different order were
    deemed different.

---
+++ packages/cli-io/test/stacks/deploy-stacks/resolve-policy-operation.test.ts
@@ -0,0 +1,76 @@
+import { resolvePolicyOperation } from "../../../src/stacks/deploy-stacks/stack-policy"
+
+const policy1 = `
+ {
+    "Statement": [
+      {
+        "Effect": "Deny",
+        "Action": ["Update:Replace", "Update:Delete"],
+        "Principal": "*",
+        "Resource": "*",
+        "Condition": {
+          "StringEquals": {
+            "ResourceType": [
+              "AWS::Cognito::UserPool",
+              "AWS::Cognito::UserPoolClient"
+            ]
+          }
+        }
+      },
+      {
+        "Effect": "Allow",
+        "Action": "Update:*",
+        "Principal": "*",
+        "Resource": "*"
+      }
+    ]
+  }
+`
+
+const policy2 = `
+ {
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Action": "Update:*",
+        "Principal": "*",
+        "Resource": "*"
+      }
+    ]
+  }
+`
+
+const policy3 = `
+ {
+    "Statement": [
+      {
+        "Action": "Update:*",
+        "Effect": "Allow",
+        "Resource": "*",
+        "Principal": "*"
+      }
+    ]
+  }
+`
+
+describe("#resolvePolicyOperation", () => {
+  test("current and updated are undefined", () => {
+    expect(resolvePolicyOperation(undefined, undefined)).toBe("retain")
+  })
+
+  test("current and updated are different", () => {
+    expect(resolvePolicyOperation(policy1, policy2)).toBe("update")
+  })
+
+  test("current is defined and updated is undefined", () => {
+    expect(resolvePolicyOperation(policy1, undefined)).toBe("delete")
+  })
+
+  test("current is undefined and updated is defined", () => {
+    expect(resolvePolicyOperation(undefined, policy2)).toBe("create")
+  })
+
+  test("current and updated are same but have different order of properties", () => {
+    expect(resolvePolicyOperation(policy2, policy3)).toBe("retain")
+  })
+})
