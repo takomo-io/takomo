cdbe005c6fe85126d331e85b9855b957579b886c
Henri Meltaus <henri@takomo.io>
2022-01-29 20:15:00 +0200

    Fix tests

--- integration-tests/deployment-targets/configs/run/get-caller-identity.js
+++ integration-tests/deployment-targets/configs/run/get-caller-identity.js
@@ -1,8 +1,10 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { STS } = require("@aws-sdk/client-sts")
-module.exports = async ({ target, credentials }) => {
-  const { Account } = await new STS({ credentials, region: "us-east-1 " })
-    .getCallerIdentity({})
-    .promise()
+module.exports = async ({ credentials }) => {
+  const { Account } = await new STS({
+    credentials,
+    region: "us-east-1",
+  }).getCallerIdentity({})
+
   return Account
 }
