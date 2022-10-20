6999d814c9a0cfc3faa0640f48e20857db0b3e11
Henri Meltaus <henri@takomo.io>
2022-10-20 21:55:29 +0300

    chore: configure building of api docs

--- ci/get-current-version.js
+++
@@ -1,4 +0,0 @@
-const fs = require("fs")
-const packageJsonContents = fs.readFileSync("./packages/takomo/package.json")
-const packageJson = JSON.parse(packageJsonContents)
-console.log(packageJson.version)
