ace85630a4c19a50418e2e3045123d49814aa585
Henri Meltaus <henri@takomo.io>
2022-10-21 22:18:16 +0300

    feat: expose public API
    Make it easier to write targets run commands with JavaScript/TypeScript.
    
    closes: #365

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,61 @@
 import "source-map-support/register"
-export {
-  Stack,
-  Resolver,
-  ResolverProvider,
-  Hook,
-  HookProvider,
-  HookOutput,
-  HookInput,
-  ResolverInput,
-  ResolverProviderSchemaProps,
-  HookOutputObject,
-  StacksContext,
-  HookOperation,
-  HookStage,
-  HookStatus,
-  StackOperationVariables,
-  HookOutputValues,
-} from "./takomo-stacks-model"
+export {} from ""
 export { CredentialManager } from "./takomo-aws-clients"
 export {
-  CommandContext,
-  OutputFormat,
-  TakomoProjectConfig,
-  Variables,
-  ContextVars,
-  EnvVars,
-  Vars,
-  TakomoProjectDeploymentTargetsConfig,
-  DeploymentTargetRepositoryConfig,
-} from "./takomo-core"
-export {
+  BaseCloudFormationStack,
   CallerIdentity,
   CloudFormationStack,
-  BaseCloudFormationStack,
+  StackCapability,
   StackDriftInformation,
   StackDriftStatus,
-  StackParameter,
-  StackCapability,
   StackOutput,
+  StackParameter,
   StackStatus,
   Tag,
 } from "./takomo-aws-model"
-export { TkmLogger, LogLevel, TemplateEngine } from "./takomo-util"
 export { run } from "./takomo-cli"
+export {
+  ConfigSetInstruction,
+  ConfigSetInstructionsHolder,
+  ConfigSetName,
+  StageName,
+} from "./takomo-config-sets"
+export {
+  CommandContext,
+  CommandRole,
+  ContextVars,
+  DeploymentTargetRepositoryConfig,
+  EnvVars,
+  OutputFormat,
+  TakomoProjectConfig,
+  TakomoProjectDeploymentTargetsConfig,
+  Variables,
+  Vars,
+} from "./takomo-core"
+export {
+  MapFunction,
+  MapFunctionProps,
+  ReduceFunction,
+  ReduceFunctionProps,
+} from "./takomo-deployment-targets-commands"
+export { DeploymentTargetConfig } from "./takomo-deployment-targets-config"
+export { DeploymentStatus, Label } from "./takomo-deployment-targets-model"
+export {
+  Hook,
+  HookInput,
+  HookOperation,
+  HookOutput,
+  HookOutputObject,
+  HookOutputValues,
+  HookProvider,
+  HookStage,
+  HookStatus,
+  Resolver,
+  ResolverInput,
+  ResolverProvider,
+  ResolverProviderSchemaProps,
+  Stack,
+  StackOperationVariables,
+  StacksContext,
+} from "./takomo-stacks-model"
+export { LogLevel, TemplateEngine, TkmLogger } from "./takomo-util"
