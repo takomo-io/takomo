c039eec1bc34c2b4ae6031028ab153bc86194f22
Henri Meltaus <henri@takomo.io>
2022-10-26 20:39:22 +0300

    fix: fix validating of command roles
    Previously, all command roles were validated by attempting to assume them.
    This was done even for stacks that were not selected to be in a stack deploy
    or undeploy operation. After this change, only command roles in stacks
    that are selected for an operation are validated.

--- integration-test/test/undeploy/undeploy-with-command-roles.test.ts
+++ integration-test/test/undeploy/undeploy-with-command-roles.test.ts
@@ -1,14 +1,31 @@
 import { executeUndeployStacksCommand } from "../../src/commands/stacks"
-import { pathToConfigs } from "../../src/util"
 import { withSingleAccountReservation } from "../../src/reservations"
+import { pathToConfigs } from "../../src/util"
 
 const projectDir = pathToConfigs("undeploy", "command-roles")
 
 describe("Undeploy", () => {
   test("Should not attempt to assume command roles of stacks that are not selected for undeploy", () =>
     executeUndeployStacksCommand({
+      commandPath: "/aaa/one.yml/eu-north-1",
       projectDir,
-      commandPath: "/aaa/one.yml",
+    })
+      .expectCommandToSkip("Skipped")
+      .expectSkippedStackResult({
+        stackPath: "/aaa/one.yml/eu-north-1",
+        message: "Stack not found",
+        stackName: "aaa-one",
+      })
+      .assert())
+
+  test("In interactive mode, should not attempt to assume command roles of stacks that are not selected for undeploy", () =>
+    executeUndeployStacksCommand({
+      projectDir,
+      interactive: true,
+      answers: {
+        confirmUndeploy: "CONTINUE",
+        chooseCommandPath: "/aaa/one.yml",
+      },
     })
       .expectCommandToSkip("Skipped")
       .expectSkippedStackResult({
