960b91a5ed340e293810f91ddd8954f39c000ce2
Henri Meltaus <henri@takomo.io>
2022-10-28 16:18:17 +0300

    feat: add commands to show and validate deployment targets configuration

---
+++ src/takomo-cli-io/deployment-targets/validate-config-io.ts
@@ -0,0 +1,21 @@
+import {
+  ValidateDeploymentTargetsConfigurationIO,
+  ValidateDeploymentTargetsConfigurationOutput,
+} from "../../takomo-deployment-targets-commands/validate-config/model"
+import { createBaseIO } from "../cli-io"
+import { IOProps } from "../stacks/common"
+
+export const createValidateDeploymentTargetsConfigurationIO = (
+  props: IOProps,
+): ValidateDeploymentTargetsConfigurationIO => {
+  const { logger } = props
+  const io = createBaseIO(props)
+
+  const printOutput = (
+    output: ValidateDeploymentTargetsConfigurationOutput,
+  ): ValidateDeploymentTargetsConfigurationOutput => {
+    return output
+  }
+
+  return { ...logger, ...io, printOutput }
+}
---
+++ src/takomo-deployment-targets-commands/validate-config/model.ts
@@ -0,0 +1,8 @@
+import { CommandInput, CommandOutput, IO } from "../../takomo-core"
+
+export type ValidateDeploymentTargetsConfigurationInput = CommandInput
+
+export type ValidateDeploymentTargetsConfigurationOutput = CommandOutput
+
+export type ValidateDeploymentTargetsConfigurationIO =
+  IO<ValidateDeploymentTargetsConfigurationOutput>
