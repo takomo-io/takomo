344f0111b1a51a5668e6c2a567b33e2e58e3cbb6
Henri Meltaus <henri@takomo.io>
2023-11-27 13:47:12 +0200

    feat: expose handlebars instance to custom helpers when using helper providers

--- src/templating/handlebars/handlebars-template-engine.ts
+++ src/templating/handlebars/handlebars-template-engine.ts
@@ -33,6 +33,77 @@ interface HandlebarsTemplateEngineProps {
   readonly logger: TkmLogger
 }
 
+/**
+ * See https://handlebarsjs.com/api-reference/compilation.html#handlebars-compile-template-options
+ */
+export interface HandlebarsCompileOptions {
+  data?: boolean
+  compat?: boolean
+  knownHelpersOnly?: boolean
+  noEscape?: boolean
+  strict?: boolean
+  assumeObjects?: boolean
+  preventIndent?: boolean
+  ignoreStandalone?: boolean
+  explicitPartialContext?: boolean
+}
+
+export type HandlebarsTemplateDelegate = (
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  context: any,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  options?: any,
+) => string
+
+/**
+ * Optional last argument for Hanblebars helpers.
+ */
+export interface HandlebarsHelperOptions {
+  /**
+   * Helper name
+   */
+  name: string
+
+  /**
+   * Hash options
+   */
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  hash: any
+
+  /**
+   * Data
+   */
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  data: any
+
+  /**
+   * Helper location in the template
+   */
+  loc: {
+    start: {
+      line: number
+      column: number
+    }
+    end: {
+      line: number
+      column: number
+    }
+  }
+}
+
+export class HandlebarsSafeString {
+  readonly #str: string
+  constructor(str: string) {
+    this.#str = str
+  }
+  toString(): string {
+    return this.#str
+  }
+  toHTML(): string {
+    return this.#str
+  }
+}
+
 export class HandlebarsTemplateEngine implements TemplateEngine {
   readonly #instance = hb.create()
   readonly #registeredPartials = new Map<string, string>()
@@ -44,6 +115,18 @@ export class HandlebarsTemplateEngine implements TemplateEngine {
     this.#logger = logger
   }
 
+  compile(
+    templateString: string,
+    options?: HandlebarsCompileOptions,
+  ): HandlebarsTemplateDelegate {
+    return this.#instance.compile(templateString, options)
+  }
+
+  safeString(str: string): HandlebarsSafeString {
+    return new HandlebarsSafeString(str)
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   registerHelper(name: string, fn: any): void {
     this.#instance.registerHelper(name, fn)
   }
@@ -95,6 +178,7 @@ export class HandlebarsTemplateEngine implements TemplateEngine {
       )
 
       return renderedTemplate
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
     } catch (e: any) {
       this.#logger.error(
         `An error occurred while rendering template from ${sourceDescription}`,
