de6c635871adb5793a02af0e0e51eae36ac3bb40
Henri Meltaus <henri@takomo.io>
2025-01-22 09:45:20 +0200

    chore: clean up code

--- src/takomo-deployment-targets-repository/index.ts
+++
@@ -1,8 +0,0 @@
-export { createDeploymentTargetRepositoryRegistry } from "./deployment-target-repository-registry.js"
-export {
-  createDeploymentTargetConfigItemSchema,
-  DeploymentTargetConfigItem,
-  DeploymentTargetRepository,
-} from "./deployment-target-repository.js"
-export { createFileSystemDeploymentTargetRepositoryProvider } from "./filesystem-deployment-target-repository.js"
-export { createOrganizationDeploymentTargetRepositoryProvider } from "./organization-deployment-target-repository.js"
