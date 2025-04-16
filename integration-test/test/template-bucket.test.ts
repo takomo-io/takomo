eb995913cc71e29a0d41ac2d2f14be9665cc7881
Henri Meltaus <henri@takomo.io>
2025-04-16 11:30:50 +0300

    fix: use region-specific template bucket url

--- integration-test/test/template-bucket.test.ts
+++ integration-test/test/template-bucket.test.ts
@@ -2,46 +2,53 @@ import { uuid } from "../../src/utils/strings.js"
 import { executeDeployStacksCommand } from "../src/commands/stacks.js"
 
 const projectDir = `${process.cwd()}/integration-test/configs/template-bucket`
-
 const bucketName = "template-bucket" + uuid()
 
 describe("Template bucket", () => {
-  test("Deploy template bucket", () =>
+  test("Deploy template buckets", () =>
     executeDeployStacksCommand({
       projectDir,
       commandPath: "/common",
-      var: [`name=${bucketName}=`],
+      var: [`name=${bucketName}`],
     })
       .expectCommandToSucceed()
       .expectStackCreateSuccess({
         stackPath: "/common/bucket.yml/eu-north-1",
         stackName: "common-bucket",
       })
+      .expectStackCreateSuccess({
+        stackPath: "/common/bucket-eu-west-1.yml/eu-west-1",
+        stackName: "common-bucket-eu-west-1",
+      })
+      .expectStackCreateSuccess({
+        stackPath: "/common/bucket-us-east-1.yml/us-east-1",
+        stackName: "common-bucket-us-east-1",
+      })
       .assert())
 
-  test("Deploy using template bucket", () =>
+  test("Deploy using template buckets", () =>
     executeDeployStacksCommand({
       projectDir,
-      commandPath: "/app/app.yml",
+      commandPath: "/app",
       var: [`name=${bucketName}`],
+      logLevel: "debug",
     })
       .expectCommandToSucceed()
       .expectStackCreateSuccess({
         stackPath: "/app/app.yml/eu-north-1",
         stackName: "app-app",
       })
-      .assert())
-
-  test("Deploy using template bucket with prefix", () =>
-    executeDeployStacksCommand({
-      projectDir,
-      commandPath: "/app/db.yml",
-      var: [`name=${bucketName}`],
-    })
-      .expectCommandToSucceed()
       .expectStackCreateSuccess({
         stackPath: "/app/db.yml/eu-north-1",
         stackName: "app-db",
+      })
+      .expectStackCreateSuccess({
+        stackPath: "/app/db-eu-west-1.yml/eu-north-1",
+        stackName: "app-db-eu-west-1",
+      })
+      .expectStackCreateSuccess({
+        stackPath: "/app/db-us-east-1.yml/eu-north-1",
+        stackName: "app-db-us-east-1",
       })
       .assert())
 })
