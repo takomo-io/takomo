fe4a331179e57be28c60862a5522da71ba49681d
Henri Meltaus <henri@takomo.io>
2025-08-05 20:39:56 +0300

    fix: upgrade dependencies (#476)

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,4 +1,4 @@
-import _date from "date-and-time"
+import { format } from "date-and-time"
 
 export const formatTimestamp = (date: Date): string =>
-  _date.format(date, "YYYY-MM-DD HH:mm:ss Z")
+  format(date, "YYYY-MM-DD HH:mm:ss Z")
